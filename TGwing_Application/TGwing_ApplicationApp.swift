77b86243c3f618a51c8bc857a6ba4ee4d943ddf9
ludswdy03 <j031024y@naver.com>
2023-10-11 23:32:32 +0900

    Initial Commit

---
+++ TGwing_Application/TGwing_ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TGwing_ApplicationApp.swift
+//  TGwing_Application
+//
+//  Created by 양진영 on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TGwing_ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
