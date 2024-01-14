54b92f62957c7729b9b74e4d31c7ce810feb8f4d
양진영[학생](소프트웨어융합대학 컴퓨터공학부) <j031024y@khu.ac.kr>
2024-01-15 00:10:43 +0900

    Profile View MVVM applied dev, PostView Created

---
+++ TGwing_Application/Core/Post/PostView.swift
@@ -0,0 +1,18 @@
+//
+//  PostView.swift
+//  TGwing_Application
+//
+//  Created by 양진영 on 1/15/24.
+//
+
+import SwiftUI
+
+struct PostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostView()
+}
---
+++ TGwing_Application/Core/Profile/UserData.swift
@@ -0,0 +1,8 @@
+//
+//  UserData.swift
+//  TGwing_Application
+//
+//  Created by 양진영 on 1/14/24.
+//
+
+import Foundation
