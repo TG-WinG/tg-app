7a0f3eed2a943fc0b7c29a075fce9bb35a779acb
양진영 <j031024y@gmail.com>
2024-02-01 05:00:52 +0900

    [PostView] Navigation Bar List 사이 공간 제거,.list 사이 여백 수정, PostlistCell 디자인 수정 [PostDetailView] ScrollView 기반 View 제작 [MainContentView] TabBar Tint Color 개선

---
+++ TGwing_Application/Core/Post/PostDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  PostDetailView.swift
+//  TGwing_Application
+//
+//  Created by 양진영 on 2/1/24.
+//
+
+import SwiftUI
+
+struct PostDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostDetailView()
+}
